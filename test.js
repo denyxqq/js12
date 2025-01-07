825837955ca6f66648db997aaf4cb304ca99b115
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2025-01-07 21:10:48 +0200

    fix

--- test.js
+++ test.js
@@ -12,44 +12,55 @@ user.premium = false;
 console.log(user);
 
 
-for (const key of Object.keys(user)) {
-    console.log(`${key}: ${user[key]}`);
-  }
+const userKey = Object.keys(user)
+  for (const key of userKey){
+  console.log(`${key} : ${user[key]}`)
+}
 
+  // #2
+  function countProps(obj){
+    let total = 0;
+    for(const key in obj){
+        total = total + 1;
+    }
+    console.log(total);
+    }
+    
+    countProps(user);
 
 //   #3
-//   const employees = {
-//     dina: 42,
-//     taras: 67,
-//    denis: 35,
-//    dima:35,
-//    nastya:40,
-//   };
+  const employees = {
+    dina: 42,
+    taras: 67,
+   denis: 35,
+   dima:35,
+   nastya:40,
+  };
 
-//   const {dina, taras, denis, dima, nastya} = employees;
+  const {dina, taras, denis, dima, nastya} = employees;
 
-//   function findBestEmployee(employees) {
-//     let max = 0;
-//     let bestWorker = "" ;
-//     for(const key in employees){
-//         if(employees[key] > max){
-//             max = employees[key];
-//             bestWorker = key;
-//         }
-//     } 
-//     return(bestWorker);
-// }
-// console.log(findBestEmployee(employees));
+  function findBestEmployee(employees) {
+    let max = 0;
+    let bestWorker = "" ;
+    for(const key in employees){
+        if(employees[key] > max){
+            max = employees[key];
+            bestWorker = key;
+        }
+    } 
+    return(bestWorker);
+}
+console.log(findBestEmployee(employees));
   //    #4
 
 const income = {
-  dina: 15000,
-  taras: 20000,
- denis: 15000,
- dima:15000,
- nastya:15000,
+  worker1: 17234,
+  worker2: 15432,
+  worker3: 21123,
+  worker4:38532,
+  worker5:12457,
 }
-const {dina, taras, denis, dima, nastya} = income;
+const {worker1, worker2, worker3, worker4, worker5} = income;
 
 function countTotalSalary(employees) {
   let maxIncome = 0;
